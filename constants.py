37050f5604164334e9be3c9299955d651926ead5
Kanaran <schwarz.niko@icloud.com>
2019-07-10 12:13:15 +0200

    draw background and snake

--- constants.py
+++ constants.py
@@ -1,2 +1,18 @@
+import pygame
+
+pygame.init()
+
+# game sizes
 GAME_WIDTH = 800
 GAME_HEIGHT = 600
+
+# color definitions
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREY = (100, 100, 100)
+
+# game colors
+COLOR_DEFAULT_BG = COLOR_GREY
+
+# sprites
+S_PLAYER = pygame.image.load("img/snake.png")
--- main.py
+++ main.py
@@ -12,21 +12,47 @@ import constants
 
 SURFACE_MAIN = None
 
+def draw_game():
+  global SURFACE_MAIN
+  
+  # clear the surface
+  SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
+  
+  #TODO draw the map
+  
+  #TODO draw the character
+  SURFACE_MAIN.blit(constants.S_PLAYER, ( 100, 100 ))
+  
+  # update the display
+  pygame.display.flip()
+  
+
 def game_main_loop():
   game_quit = False
   
   while not game_quit:
-    #TODO collect input
+    # collect input
+    events_list = pygame.event.get()
     
-    #TODO process input
+    # process input
+    for event in events_list:
+      if event.type == pygame.QUIT:
+        game_quit = True
     
     #TODO draw
+    draw_game()
     
-  #TODO quit
+  # quit
+  pygame.quit()
+  exit()
 
 def game_initialize():
   global SURFACE_MAIN
   
   pygame.init()
 
-  SURFACE_MAIN = pygame.display.set_mode( (800, 600) )
+  SURFACE_MAIN = pygame.display.set_mode( (constants.GAME_WIDTH, constants.GAME_HEIGHT) )
+  
+if __name__ == '__main__':
+  game_initialize()
+  game_main_loop()
